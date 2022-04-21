e15d35df95edaef2e5b28c7e493f9d29f2e2143c
valleyman89 <dev@stevenrolph.com>
2022-04-21 12:19:59 +0100

    define input box

--- src/components/CreateReply.jsx
+++ src/components/CreateReply.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Input from "./common/input";
 function CreateReply(props) {
-  return <div className="box">hello</div>;
+  return (
+    <div className="box">
+      <Input></Input>
+    </div>
+  );
 }
 
 export default CreateReply;
---
+++ src/components/common/input.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function input(props) {
+  return (
+    <div>
+      <textarea className="input" placeholder="Add a comment..."></textarea>
+    </div>
+  );
+}
+
+export default input;
