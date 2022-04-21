2148b99ef39459b24c41dc04a012624cb3cca8c5
valleyman89 <dev@stevenrolph.com>
2022-04-21 14:59:54 +0100

    add grid and layout

--- src/components/CreateReply.jsx
+++ src/components/CreateReply.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import Input from "./common/input";
 function CreateReply(props) {
   return (
-    <div className="box">
-      <Input></Input>
+    <div className="box reply">
+      <div className="reply-content">
+        <Input></Input>
+      </div>
+      <div className="reply-info">avatar</div>
+      <div className="reply-action">
+        <button className="btn-send btn-score">send</button>
+      </div>
     </div>
   );
 }
