41cdbc5cf37fa8cc73facee42608d78db9a19383
valleyman89 <dev@stevenrolph.com>
2022-05-03 15:09:37 +0100

    define add comment functionality

--- src/components/CreateReply.jsx
+++ src/components/CreateReply.jsx
@@ -1,17 +1,55 @@
-import React from "react";
-import Input from "./common/input";
-function CreateReply(props) {
+import React, { useState, useEffect } from "react";
+import { getUsers } from "../services/user";
+
+const CreateReply = (props) => {
+  const { onReply, comments } = props;
+  const { currentUser } = getUsers();
+
+  const [commentText, setCommentText] = useState("");
+  const incrementId = comments.length + 1;
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    const newComment = {
+      id: incrementId,
+      content: commentText,
+      createdAt: "a moment ago",
+      score: 0,
+      user: {
+        image: {
+          png: "./images/avatars/image-" + currentUser.username + ".png",
+          webp: "./images/avatars/image-" + currentUser.username + ".webp",
+        },
+        username: currentUser.username,
+      },
+      replies: [],
+    };
+
+    onReply(newComment);
+  };
+
   return (
-    <div className="box reply">
-      <div className="reply-content">
-        <Input></Input>
+    <form onSubmit={handleOnSubmit}>
+      <div className="box reply">
+        <div className="reply-content">
+          <textarea
+            className="input"
+            name="commentTextArea"
+            type="text"
+            placeholder="Add a comment..."
+            onChange={(e) => setCommentText(e.target.value)}
+          />
+        </div>
+        <div className="reply-info comment__avatar">
+          <img src={currentUser.image.png} alt="profile" />
+        </div>
+        <div className="reply-action">
+          <button className="btn-send btn-score">send</button>
+        </div>
       </div>
-      <div className="reply-info">avatar</div>
-      <div className="reply-action">
-        <button className="btn-send btn-score">send</button>
-      </div>
-    </div>
+    </form>
   );
-}
+};
 
 export default CreateReply;
