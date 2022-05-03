eee8158e6077298c3be4e4e9d06a6a6fbf6de9ae
valleyman89 <dev@stevenrolph.com>
2022-05-03 15:08:36 +0100

    define logic for showing edit/reply icon

--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import { ReactComponent as IconReply } from "../svg/icon-reply.svg";
+import { ReactComponent as IconEdit } from "../svg/icon-edit.svg";
+import { getUsers } from "../services/user";
+
 class Reply extends Component {
   render() {
+    const { user } = this.props;
+    const { currentUser } = getUsers();
+
     return (
       <div className="action action__reply comment-action-3">
         <button>
-          <IconReply /> Reply
+          {user === currentUser.username ? <IconEdit /> : <IconReply />}
+          {user === currentUser.username ? " Edit" : " Reply"}
         </button>
       </div>
     );
