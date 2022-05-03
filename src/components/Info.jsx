1ac52cbc1d35f3efe9f1996b50c41b9963a8c23f
valleyman89 <dev@stevenrolph.com>
2022-05-03 15:09:49 +0100

    define you identifier

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
+import { getUsers } from "../services/user";
 
 class Info extends Component {
   render() {
     const { user, date, avatar } = this.props;
-
+    const { currentUser } = getUsers();
     return (
       <React.Fragment>
         <div className="comment__avatar comment-info-1">
           <img src={avatar} alt="profile" />
         </div>
         <div className="comment__username comment-info-2">{user}</div>
-        <div className="comment-info-3">{date}</div>
+        <div className="comment-info-3">
+          {user === currentUser.username ? (
+            <span className="comment__currentuser">you</span>
+          ) : null}
+        </div>
+        <div className="comment-info-4">{date}</div>
       </React.Fragment>
     );
   }
